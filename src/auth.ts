c8c64e8cce6ba9239df7de0d8ea498f2799160a3
Cong Dao <congdaovan94@gmail.com>
2024-10-09 13:56:42 -0400

    Fix auth

--- src/auth.ts
+++ src/auth.ts
@@ -10,7 +10,7 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET || !GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   throw new Error('Missing github auth credentials');
 }
 
