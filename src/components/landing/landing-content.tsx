a067f388222df62bded5ba3e033f7a5fd94a5cd2
Cong Dao <congdaovan94@gmail.com>
2024-11-03 16:01:11 -0500

    Fix typo

--- src/components/landing/landing-content.tsx
+++ src/components/landing/landing-content.tsx
@@ -4,7 +4,7 @@ export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
       <h2 className="text-center text-3xl text-white font-extrabold mb-10">
-        Why Feedback?
+        Why Feedback ?
       </h2>
       <p className=" text-center text-zinc-400 text-sm">
       Feedback is designed to streamline customer interactions, reduce response times, and increase customer satisfaction. With our easy-to-navigate platform, your team can focus on providing top-notch support without getting bogged down by complex tools.
